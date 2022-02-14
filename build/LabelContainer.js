d70b551027c0d3afb754be6f50e645c3a79dbe66
nordic96 <rhrlgns96@gmail.com>
2022-02-14 09:32:29 +0800

    add build files

--- build/LabelContainer.js
+++ build/LabelContainer.js
@@ -1,71 +1,57 @@
-import { Labels } from "./types";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * @author Stephen Ko
  * @since 14 Feb 2022
  * Centralised Label Container Class to store and retrieve label strings
  */
 class LabelContainer {
-    /** which page the component is located */
-    private page: string;
-    /** language preference from the browser (user/local) */
-    private language: string;
-    /** labels object to be used to retrieve labels */
-    private labels: Labels;
-    /** singleton instance */
-    private static instance: LabelContainer;
-
-    private constructor () {
+    constructor() {
         /** default language set to Eng */
-        let language: string = 'en';
+        let language = 'en';
         this.language = language;
     }
-
     /** Singleton get instance method */
-    public static getInstance(): LabelContainer {
+    static getInstance() {
         if (!this.instance) {
             this.instance = new LabelContainer();
         }
         return this.instance;
     }
-
     /** setter method for labels property */
-    public setLabels(labels: Labels) {
+    setLabels(labels) {
         this.labels = labels;
     }
-
     /** getter method for labels property */
-    public getLabels(): Labels {
+    getLabels() {
         return this.labels;
     }
-
     /** setter method for language property */
-    public setLanguage(language: string) {
+    setLanguage(language) {
         this.language = language;
     }
-
     /** getter method for language property */
-    public getLanguage() {
+    getLanguage() {
         return this.language;
     }
-
-    public getLabel(key: string) {
+    getLabel(key) {
         try {
-            let label: string;
+            let label;
             /** If Page & Language Specific Label exists, return this label */
-            if (this.page) label = this.labels[this.page][this.language][key];
+            if (this.page)
+                label = this.labels[this.page][this.language][key];
             /** If no page specified in the instnace, return the global label */
             label = this.labels.GLOBAL[this.language][key];
-
             /** If both cases above fails to load the label, return the key */
-            if (label) return label;
+            if (label)
+                return label;
             return key;
-        } catch (e) {
+        }
+        catch (e) {
             /** Any exception caught from trying the label retrieval, return the key */
             console.debug(e.message);
             return key;
         }
     }
 }
-
-export default LabelContainer;
+exports.default = LabelContainer;
