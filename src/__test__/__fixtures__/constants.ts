a3e4e8a142619614c989ccac8f05df1e72151cbd
nordic96 <rhrlgns96@gmail.com>
2022-10-18 15:09:38 +0800

    refactor stub label file to fixtures dir

---
+++ src/__test__/__fixtures__/constants.ts
@@ -0,0 +1,20 @@
+import { Labels } from '../../types';
+
+export const StubLabel: Labels = {
+    GLOBAL: {
+        en: {
+            key_a: 'Hello World',
+        },
+        ko: {
+            key_a: '안녕',
+        },
+    },
+    page_a: {
+        en: {
+            key_a: 'Hello Page A',
+        },
+        cn: {
+            key_a: 'Da Jia Hao',
+        },
+    },
+};
